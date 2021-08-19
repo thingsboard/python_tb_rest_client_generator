81520aa006a5aad1da5dcb57299962cf3ad82dcd
samson0v <vitalikbidochka@gmail.com>
2021-08-19 12:45:11 +0300

    Fixed double processing the file

--- generator.py
+++ generator.py
@@ -117,126 +117,131 @@ class Generator:
     def _generate_files(self, path: str, ce_files: [File], pe_files: [File], mode: str):
         files_set = pe_files + ce_files
 
+        proceed_files = []
+
         ce_files_set = tuple([x.filename for x in ce_files])
         pe_files_set = tuple([x.filename for x in pe_files])
 
         for file in files_set:
-            if file.filename in ce_files_set and file.filename in pe_files_set:
-                handle_pe = self._read_file(
-                    pe_files[pe_files_set.index(file.filename)].full_file_path)
-                handle_ce = self._read_file(
-                    ce_files[ce_files_set.index(file.filename)].full_file_path)
+            if file.filename not in proceed_files:
+                if file.filename in ce_files_set and file.filename in pe_files_set:
+                    handle_pe = self._read_file(
+                        pe_files[pe_files_set.index(file.filename)].full_file_path)
+                    handle_ce = self._read_file(
+                        ce_files[ce_files_set.index(file.filename)].full_file_path)
 
-                if handle_ce == handle_pe:
-                    self._write_file(path, file, handle_pe, 'ce')
+                    if handle_ce == handle_pe:
+                        self._write_file(path, file, handle_pe, 'ce')
 
-                    if mode == 'models':
-                        self._write_init_file(file, 'tb_rest_client/models/models_', 'ce')
+                        if mode == 'models':
+                            self._write_init_file(file, 'tb_rest_client/models/models_', 'ce')
 
-                    if mode == 'controllers':
-                        self._write_init_file(file, 'tb_rest_client/api/api_', 'ce')
+                        if mode == 'controllers':
+                            self._write_init_file(file, 'tb_rest_client/api/api_', 'ce')
 
-                        if file.class_name:
-                            klass = self._import_class('tb_rest_client.api.api_ce.' + file.filename.split('.')[0],
-                                                       file.class_name)
-                            method_list = filter(lambda x: x is not None, list(
-                                map(lambda x: getattr(klass, x) if callable(getattr(klass, x)) and x.startswith(
-                                    '__') is False and '_with_http_info' not in x else None, dir(klass))))
+                            if file.class_name:
+                                klass = self._import_class('tb_rest_client.api.api_ce.' + file.filename.split('.')[0],
+                                                           file.class_name)
+                                method_list = filter(lambda x: x is not None, list(
+                                    map(lambda x: getattr(klass, x) if callable(getattr(klass, x)) and x.startswith(
+                                        '__') is False and '_with_http_info' not in x else None, dir(klass))))
 
-                            function_list = self._generate_functions(file.filename.split('.')[0], method_list)
-                            s = ''
-                            for i in function_list:
-                                s += i.str_function + '\n'
-                            self._rest_client_base.methods_section = s
-                            self._rest_client_base.init = klass
+                                function_list = self._generate_functions(file.filename.split('.')[0], method_list)
+                                s = ''
+                                for i in function_list:
+                                    s += i.str_function + '\n'
+                                self._rest_client_base.methods_section = s
+                                self._rest_client_base.init = klass
 
-                else:
-                    self._write_file(path, file, handle_ce, 'ce')
-                    self._write_file(path, file, handle_pe, 'pe')
-
-                    if mode == 'models':
-                        self._write_init_file(file, 'tb_rest_client/models/models_', 'ce')
-                        self._write_init_file(file, 'tb_rest_client/models/models_', 'pe')
-
-                    if mode == 'controllers':
-                        self._write_init_file(file, 'tb_rest_client/api/api_', 'ce')
-                        self._write_init_file(file, 'tb_rest_client/api/api_', 'pe')
-
-                        if file.class_name:
-                            ce_file = ce_files[ce_files_set.index(file.filename)]
-                            pe_file = pe_files[pe_files_set.index(file.filename)]
-
-                            # TODO: remake writing init file strategy
-                            self._write_init_file(ce_file, 'tb_rest_client/api/api_', 'ce')
-                            self._write_init_file(pe_file, 'tb_rest_client/api/api_', 'pe')
-
-                            pe_klass = self._import_class(
-                                f'tb_rest_client.api.api_pe.' + pe_file.filename.split('.')[0], pe_file.class_name)
-                            ce_klass = self._import_class(
-                                f'tb_rest_client.api.api_ce.' + ce_file.filename.split('.')[0], ce_file.class_name)
-
-                            pe_method_list = filter(lambda x: x is not None, list(
-                                map(lambda x: getattr(pe_klass, x) if callable(getattr(pe_klass, x)) and x.startswith(
-                                    '__') is False and '_with_http_info' not in x else None, dir(pe_klass))))
-                            pe_function_list = self._generate_functions(pe_file.filename.split('.')[0], pe_method_list)
-                            pe_function_names_dict = {func.name: func for func in pe_function_list}
-
-                            ce_method_list = filter(lambda x: x is not None, list(
-                                map(lambda x: getattr(ce_klass, x) if callable(getattr(ce_klass, x)) and x.startswith(
-                                    '__') is False and '_with_http_info' not in x else None, dir(ce_klass))))
-                            ce_function_list = self._generate_functions(ce_file.filename.split('.')[0], ce_method_list)
-                            ce_function_names_dict = {func.name: func for func in ce_function_list}
-
-                            full_functions = set(ce_function_names_dict.keys()) & set(pe_function_names_dict.keys())
-                            with_the_same_params = [func[0] for func in list(
-                                filter(lambda x: x[0].params == x[1].params,
-                                       [(ce_function_names_dict[function_name], pe_function_names_dict[function_name])
-                                        for function_name in full_functions]))]
-                            not_the_same_function = filter(lambda x: x not in with_the_same_params, full_functions)
-
-                            try:
-                                for i in with_the_same_params:
-                                    self._rest_client_base.methods_section = i.str_function + '\n'
-
-                                for i in not_the_same_function:
-                                    if ce_function_names_dict.get(i):
-                                        self._rest_client_ce.methods_section = ce_function_names_dict[
-                                                                                   i].str_function + '\n'
-                                        self._rest_client_base.init = ce_klass
-                                    if pe_function_names_dict.get(i):
-                                        self._rest_client_pe.methods_section = pe_function_names_dict[
-                                                                                   i].str_function + '\n'
-                                        self._rest_client_pe.init = pe_klass
-                            except Exception as e:
-                                print(e)
-            else:
-                handle_data = self._read_file(file.full_file_path)
-                self._write_file(path, file, handle_data, file.version)
-
-                if mode == 'models':
-                    self._write_init_file(file, 'tb_rest_client/models/models_', file.version)
-
-                if mode == 'controllers':
-                    self._write_init_file(file, 'tb_rest_client/api/api_', file.version)
-
-                    klass = self._import_class(f'tb_rest_client.api.api_{file.version}.' + file.filename.split('.')[0],
-                                               ''.join(word.title() for word in file.filename.split('_')).split('.')[0])
-                    method_list = filter(lambda x: x is not None, list(
-                        map(lambda x: getattr(klass, x) if callable(getattr(klass, x)) and x.startswith(
-                            '__') is False and '_with_http_info' not in x else None, dir(klass))))
-
-                    function_list = self._generate_functions(file.filename.split('.')[0], method_list)
-
-                    s = ''
-                    for i in function_list:
-                        s += i.str_function + '\n'
-
-                    if file.version == 'ce':
-                        self._rest_client_ce.methods_section = s
-                        self._rest_client_base.init = klass
                     else:
-                        self._rest_client_pe.methods_section = s
-                        self._rest_client_pe.init = klass
+                        self._write_file(path, file, handle_ce, 'ce')
+                        self._write_file(path, file, handle_pe, 'pe')
+
+                        if mode == 'models':
+                            self._write_init_file(file, 'tb_rest_client/models/models_', 'ce')
+                            self._write_init_file(file, 'tb_rest_client/models/models_', 'pe')
+
+                        if mode == 'controllers':
+                            self._write_init_file(file, 'tb_rest_client/api/api_', 'ce')
+                            self._write_init_file(file, 'tb_rest_client/api/api_', 'pe')
+
+                            if file.class_name:
+                                ce_file = ce_files[ce_files_set.index(file.filename)]
+                                pe_file = pe_files[pe_files_set.index(file.filename)]
+
+                                # TODO: remake writing init file strategy
+                                self._write_init_file(ce_file, 'tb_rest_client/api/api_', 'ce')
+                                self._write_init_file(pe_file, 'tb_rest_client/api/api_', 'pe')
+
+                                pe_klass = self._import_class(
+                                    f'tb_rest_client.api.api_pe.' + pe_file.filename.split('.')[0], pe_file.class_name)
+                                ce_klass = self._import_class(
+                                    f'tb_rest_client.api.api_ce.' + ce_file.filename.split('.')[0], ce_file.class_name)
+
+                                pe_method_list = filter(lambda x: x is not None, list(
+                                    map(lambda x: getattr(pe_klass, x) if callable(getattr(pe_klass, x)) and x.startswith(
+                                        '__') is False and '_with_http_info' not in x else None, dir(pe_klass))))
+                                pe_function_list = self._generate_functions(pe_file.filename.split('.')[0], pe_method_list)
+                                pe_function_names_dict = {func.name: func for func in pe_function_list}
+
+                                ce_method_list = filter(lambda x: x is not None, list(
+                                    map(lambda x: getattr(ce_klass, x) if callable(getattr(ce_klass, x)) and x.startswith(
+                                        '__') is False and '_with_http_info' not in x else None, dir(ce_klass))))
+                                ce_function_list = self._generate_functions(ce_file.filename.split('.')[0], ce_method_list)
+                                ce_function_names_dict = {func.name: func for func in ce_function_list}
+
+                                full_functions = set(ce_function_names_dict.keys()) & set(pe_function_names_dict.keys())
+                                with_the_same_params = [func[0] for func in list(
+                                    filter(lambda x: x[0].params == x[1].params,
+                                           [(ce_function_names_dict[function_name], pe_function_names_dict[function_name])
+                                            for function_name in full_functions]))]
+                                not_the_same_function = filter(lambda x: x not in with_the_same_params, full_functions)
+
+                                try:
+                                    for i in with_the_same_params:
+                                        self._rest_client_base.methods_section = i.str_function + '\n'
+
+                                    for i in not_the_same_function:
+                                        if ce_function_names_dict.get(i):
+                                            self._rest_client_ce.methods_section = ce_function_names_dict[
+                                                                                       i].str_function + '\n'
+                                            self._rest_client_base.init = ce_klass
+                                        if pe_function_names_dict.get(i):
+                                            self._rest_client_pe.methods_section = pe_function_names_dict[
+                                                                                       i].str_function + '\n'
+                                            self._rest_client_pe.init = pe_klass
+                                except Exception as e:
+                                    print(e)
+                else:
+                    handle_data = self._read_file(file.full_file_path)
+                    self._write_file(path, file, handle_data, file.version)
+
+                    if mode == 'models':
+                        self._write_init_file(file, 'tb_rest_client/models/models_', file.version)
+
+                    if mode == 'controllers':
+                        self._write_init_file(file, 'tb_rest_client/api/api_', file.version)
+
+                        klass = self._import_class(f'tb_rest_client.api.api_{file.version}.' + file.filename.split('.')[0],
+                                                   ''.join(word.title() for word in file.filename.split('_')).split('.')[0])
+                        method_list = filter(lambda x: x is not None, list(
+                            map(lambda x: getattr(klass, x) if callable(getattr(klass, x)) and x.startswith(
+                                '__') is False and '_with_http_info' not in x else None, dir(klass))))
+
+                        function_list = self._generate_functions(file.filename.split('.')[0], method_list)
+
+                        s = ''
+                        for i in function_list:
+                            s += i.str_function + '\n'
+
+                        if file.version == 'ce':
+                            self._rest_client_ce.methods_section = s
+                            self._rest_client_base.init = klass
+                        else:
+                            self._rest_client_pe.methods_section = s
+                            self._rest_client_pe.init = klass
+
+                proceed_files.append(file.filename)
 
         with open('tb_rest_client/rest_client_base.py', 'w') as f:
             f.writelines(self._rest_client_base.generate_file())
